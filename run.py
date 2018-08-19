f7ec9e8674e1df8b25755679e9b2ead79abf87dc
Keviner123 <kevin@kevinfrost.dk>
2018-08-20 00:21:38 +0200

    Added ffmpeg

--- run.py
+++ run.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 import urllib.request
+import os
 
 __author__ = "Keviner123"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
 
-def url_is_alive(url):
+STARTURL = "https://drod01o-vh.akamaihd.net/i/all/clear/streaming/43/5b5fbc1a6187a61a3411b343/Limbo-III--5-_29ac80d1b3c8424fb432bd8aac9e9ecc_,364,576,1128,2392,.mp4.csmil/segment"
+ENDURL = "_3_av.ts"
+
+def GetDownloadedClips():
+	s = ""
+	for root, dirs, files in os.walk("temp"):  
+		for filename in sorted(files, key=lambda a: int(a.split(".")[0])):
+			s+=str('temp/'+filename+"|")
+	return('ffmpeg.exe -i "concat:'+s[:-1]+'" -c copy output.mp4')
+
+def GetUrlIsAlive(url):
     request = urllib.request.Request(url)
     try:
         urllib.request.urlopen(request)
@@ -14,18 +25,26 @@ def url_is_alive(url):
     except urllib.request.HTTPError:
         return False
 
+def GetClipAmounts():
+	i = 1;
+	while(True):
+		if (GetUrlIsAlive(STARTURL+str(i)+ENDURL) == False):
+			return(i)
+		i = i+1
 
+def ClearTemp():
+	filelist = [f for f in os.listdir("temp")]
+	for f in filelist:
+		os.remove(os.path.join("temp", f))
+		
 def main():
-	STARTURL = "https://drod08s-vh.akamaihd.net/i/all/clear/streaming/65/5b6a727d6187ac0c9876fb65/Limbo-III--10-_68d9e80559b9478a96059df5a547361e_,364,576,1128,2392,.mp4.csmil/segment"
-	ENDURL = "_3_av.ts"
+	ClearTemp()
 
-
-	for i in range(1, 100):
-		if (url_is_alive(STARTURL+str(i)+ENDURL) == True):
+	for i in range(1, GetClipAmounts()):
+		if (GetUrlIsAlive(STARTURL+str(i)+ENDURL) == True):
 			URL = STARTURL+str(i)+ENDURL
-
-			urllib.request.urlretrieve(URL, str(i)+".mp4")
-
-
+			urllib.request.urlretrieve(URL, "temp/"+str(i)+".mp4")
+	os.system((GetDownloadedClips()))
+	ClearTemp()
 if __name__ == "__main__":
 	main()
